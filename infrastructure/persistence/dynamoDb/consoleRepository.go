9bc77cfda31c659822871c036eefffd15d5af405
64070103 Wipupat Chomthaworn <64070103@it.kmitl.ac.th>
2024-03-01 00:05:54 +0700

    i think Console service work now

---
+++ infrastructure/persistence/dynamoDb/consoleRepository.go
@@ -0,0 +1,110 @@
+package dynamoDb
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Kenmuraki5/kro-backend.git/domain/entity"
+	"github.com/Kenmuraki5/kro-backend.git/domain/restmodel"
+	"github.com/google/uuid"
+
+	// "github.com/Kenmuraki5/kro-backend.git/domain/repository"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+type DynamoDBConsoleRepository struct {
+	Client *dynamodb.Client
+}
+
+func NewDynamoDBConsoleRepository(client *dynamodb.Client) *DynamoDBConsoleRepository {
+	return &DynamoDBConsoleRepository{Client: client}
+}
+
+func (repo *DynamoDBConsoleRepository) GetAllConsoles() ([]*entity.Console, error) {
+	input := &dynamodb.ScanInput{
+		TableName: aws.String("Consoles"),
+	}
+	result, err := repo.Client.Scan(context.TODO(), input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan DynamoDB table: %v", err)
+	}
+	var consoles []*entity.Console
+	for _, item := range result.Items {
+		fmt.Println(item)
+		var console entity.Console
+		err := attributevalue.UnmarshalMap(item, &console)
+		if err != nil {
+			return nil, err
+		}
+		consoles = append(consoles, &console)
+	}
+	fmt.Println(consoles)
+	return consoles, nil
+}
+
+func (repo *DynamoDBConsoleRepository) AddConsole(console restmodel.Console) (*restmodel.Console, error) {
+	item, err := attributevalue.MarshalMap(console)
+	item["Id"] = &types.AttributeValueMemberS{Value: uuid.NewString()}
+	fmt.Print(item)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = repo.Client.PutItem(context.Background(), &dynamodb.PutItemInput{
+		TableName: aws.String("Consoles"),
+		Item:      item,
+	})
+	if err != nil {
+		fmt.Printf("Couldn't add item to table. Here's why: %v\n", err)
+		return nil, err
+	}
+	return &console, nil
+}
+
+// update
+func (repo *DynamoDBConsoleRepository) UpdateConsole(updatedConsole entity.Console) (*entity.Console, error) {
+	if updatedConsole.Id == "" {
+		return nil, fmt.Errorf("cannot update console without a valid ID")
+	}
+
+	item, err := attributevalue.MarshalMap(updatedConsole)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = repo.Client.PutItem(context.Background(), &dynamodb.PutItemInput{
+		TableName: aws.String("Consoles"),
+		Item:      item,
+	})
+	if err != nil {
+		fmt.Printf("Couldn't update item in table. Here's why: %v\n", err)
+		return nil, err
+	}
+
+	return &updatedConsole, nil
+}
+
+// delete
+func (repo *DynamoDBConsoleRepository) DeleteConsole(id string) error {
+	if id == "" {
+		return fmt.Errorf("cannot delete console without a valid ID")
+	}
+
+	keyCondition := map[string]types.AttributeValue{
+		"Id": &types.AttributeValueMemberS{Value: id},
+	}
+
+	_, err := repo.Client.DeleteItem(context.Background(), &dynamodb.DeleteItemInput{
+		TableName: aws.String("Consoles"),
+		Key:       keyCondition,
+	})
+	if err != nil {
+		fmt.Printf("Couldn't delete item from table. Here's why: %v\n", err)
+		return fmt.Errorf("failed to delete console: %v", err)
+	}
+
+	return err
+}
---
+++ interface/api/rest/consoleController.go
@@ -0,0 +1,87 @@
+package rest
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Kenmuraki5/kro-backend.git/application/interfaces"
+	"github.com/Kenmuraki5/kro-backend.git/domain/entity"
+	"github.com/Kenmuraki5/kro-backend.git/domain/restmodel"
+	"github.com/gin-gonic/gin"
+)
+
+type ConsoleController struct {
+	service interfaces.ConsoleService
+}
+
+func NewConsoleController(service interfaces.ConsoleService) *ConsoleController {
+	return &ConsoleController{
+		service: service,
+	}
+}
+
+// set up router
+func (gc *ConsoleController) SetupRoutes(router *gin.Engine) {
+	consoleGroup := router.Group("/consoles")
+	{
+		consoleGroup.GET("", gc.GetAllConsolesHandler)
+		consoleGroup.POST("/addConsole", gc.AddConsoleHandler)
+		consoleGroup.PUT("/updateConsole", gc.UpdateConsoleHandler)
+		consoleGroup.DELETE("/deleteConsole/:id", gc.DeleteConsole)
+	}
+}
+
+func (controller *ConsoleController) GetAllConsolesHandler(c *gin.Context) {
+	consoles, err := controller.service.GetAllConsoles()
+	fmt.Println(consoles)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch consoles"})
+		return
+	}
+
+	c.JSON(http.StatusOK, consoles)
+}
+
+func (controller *ConsoleController) AddConsoleHandler(c *gin.Context) {
+	var newConsole restmodel.Console
+	if err := c.ShouldBindJSON(&newConsole); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	addedConsole, err := controller.service.AddConsole(newConsole)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, addedConsole)
+}
+
+func (controller *ConsoleController) UpdateConsoleHandler(c *gin.Context) {
+	var updatedConsole entity.Console
+	if err := c.ShouldBindJSON(&updatedConsole); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	consoles, err := controller.service.UpdateConsole(updatedConsole)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update console"})
+		return
+	}
+
+	c.JSON(http.StatusOK, consoles)
+}
+
+func (controller *ConsoleController) DeleteConsole(c *gin.Context) {
+	id := c.Param("id")
+
+	err := controller.service.DeleteConsole(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete console"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Console item delete successfully"})
+}
